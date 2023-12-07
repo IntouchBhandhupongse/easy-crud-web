90bf58f7c0f09cf1d7ddafcc08a5f8381dc8db68
IntouchTechex <intouch@techex.co.th>
2023-12-08 00:34:38 +0700

    Edit Name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/easy-crud-web'),
+      dir: require('path').join(__dirname, './coverage/my-habit-tracker-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
